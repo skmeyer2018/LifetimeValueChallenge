4fea99e2c8d7fa26bbcafea915e1ed6eca45986b
Stephen Kleimeyer <37917233+skmeyer2018@users.noreply.github.com>
2021-10-07 11:28:54 -0400

    Add files via upload

--- Index.php
+++ Index.php
@@ -1,5 +1,29 @@
-<?php
+<!DOCTYPE html>
+<HTML>
+<HEAD>
+<TITLE>URL REDUCER</TITLE>
+<STYLE>
+ div.scroll {
+ background-color: #55ff00;
+  width: 800px;
+  height: 300px;
+  overflow: auto;
+  text-align: justify;
+ }
+</STYLE>
+</HEAD>
+<BODY>
+<H1>URL REDUCER</H1>
+<b>PLEASE ENTER A URL TO BE REDUCED</b></br>
+<FORM method="POST">
+<INPUT type="TEXT" name="origURL" id="origURL" size="100"><br>
+<INPUT type="submit" VALUE="CONVERT TO SMALLER URL" style="width:200px;">
+</FORM>
+<b>SMALLER URL:</b><br>
 
+
+
+<?php
 
 class SmallURL{
 
@@ -55,7 +79,7 @@ class SmallURL{
      {
       while($row = $selectResult->fetch_assoc())
        {
-          echo "$origURL ALREADY EXISTS as ". $row['ShortURL'] . "<br>";
+          echo "$origURL ALREADY EXISTS as <a href='$origURL'>". $row['ShortURL'] . "</a>";
           $hitCount=$row['HitCount'];
           ++$hitCount;
           $sqlUpdateHitCount="UPDATE shorterurls SET HitCount = $hitCount WHERE OrigURL = '$origURL' ";
@@ -67,7 +91,19 @@ class SmallURL{
      $sqlInsert="INSERT INTO shorterurls (OrigURL, ShortURL, DateCreated) VALUES ('$origURL', '$newURL', '$dateURLCreated')";
      if ($conn->query($sqlInsert) === TRUE)
       {
-        echo "New URL created successfully as  $newURL <br>";
+        echo "New URL created successfully as <a href='$origURL'> $newURL </a><br>";
+        echo "<b>LIST OF TOP 100 URLs</b><br>";
+        echo "<DIV class='scroll'>";
+        $sqlSelect="SELECT OrigURL, ShortURL FROM shorterurls LIMIT 100";
+        $selectResult=$conn->query($sqlSelect);
+        if ($selectResult->num_rows >0)
+        {
+         while($row = $selectResult->fetch_assoc())
+          {
+             echo $row['OrigURL'] . " ====>    <a href='" . $row['OrigURL']. "'>" . $row['ShortURL'] . "</a><br>";
+          }
+        } 
+       echo "</DIV><br>";     
       } 
     else
      {
@@ -91,29 +127,11 @@ class SmallURL{
    die("Connection failed: " . $conn->connect_error);
   }
 
-  $origURL="";
+  $origURL=$_POST['origURL'];
   $URL1= new SmallURL();
   $URL1->create_new_URL($origURL, date("m/d/Y H:m:s"), $conn);
 
-  $origURL="http://www.facebook.com";
-  $URL2= new SmallURL();
-  $URL2->create_new_URL($origURL, date("m/d/Y H:m:s"), $conn);
-
-  $origURL="http://www.linkedin.com";
-  $URL3= new SmallURL();
-  $URL3->create_new_URL($origURL, date("m/d/Y H:m:s"), $conn);
-
-  $origURL="http://www.facebook.com";
-  $URL4= new SmallURL();
-  $URL4->create_new_URL($origURL, date("m/d/Y H:m:s"), $conn);
-
-  $origURL="hello world";
-  $URL5= new SmallURL();
-  $URL5->create_new_URL($origURL, date("m/d/Y H:m:s"), $conn);
-
-
-
-
-  
-  
 ?>
+
+</BODY>
+</HTML>
